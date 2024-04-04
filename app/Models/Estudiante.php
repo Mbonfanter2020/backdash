10f7d732644fecce7f7a22c37b7d8e1b15e2c59c
Arturo Batista <batistaquiroz@gmail.com>
2024-04-03 20:58:24 -0500

    Terminando API estudiantes, con validaciones generales

--- app/Models/Estudiante.php
+++ app/Models/Estudiante.php
@@ -11,8 +11,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Estudiante extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'codigo';
+    public $incrementing = false;
+    protected $fillable = ['codigo', 'nombre', 'email', 'fechaN', 'programa_id'];
 
     public function programa(): BelongsTo {
         return $this->belongsTo(Programa::class);
     }
+
 }
